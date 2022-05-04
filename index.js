ed65014c7c2c2b7e0bcc2a5fd9c73ad0d434786b
shahnmah <shahn.mah@gmail.com>
2022-05-04 06:37:39 +0600

    server added

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+// middleware
+app.use(cors())
+app.use(express())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.1psxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service')
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        })
+        app.get('/service/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query ={_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+// post 
+app.post('/service', async(req, res) =>{
+    const newService = req.body;
+    const result = await serviceCollection.insertOne(newService)
+    res.send(result)
+})
+
+
+app.get('/', (req, res) => {
+    res.send('hi there')
+})
+app.listen(port, () => {
+    console.log('Listening to port', port)
+})
